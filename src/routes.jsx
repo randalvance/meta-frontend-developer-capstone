c072fdefeb9ed0a886c792481ff363b322d88c0f
Randal Vance Cunanan <randal@randalvance.net>
2024-05-04 21:46:00 +0800

    Add Booking Page

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from './HomePage';
+import BookingPage from './BookingPage';
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/booking" element={<BookingPage />} />
         </Routes>
     </BrowserRouter>
   );
