88b68cdeb32f62e2eab5a7bd591280d5eeaf7b9e
Randal Vance Cunanan <randal@randalvance.net>
2024-08-02 13:47:25 +0800

    More Tests

--- src/pages/BookingPage/components/BookingForm.spec.jsx
+++ src/pages/BookingPage/components/BookingForm.spec.jsx
@@ -4,7 +4,13 @@ import { BookingForm } from './BookingForm';
 import { expect, test } from 'vitest';
 
 test('Renders the BookForm heading', () => {
-    render(<BookingForm state={initialState} dispatch={() => {}} />);
+    render(<BookingForm state={initialState} dispatch={() => {}} onSubmit={() => {}} />);
     const headingElement= screen.getByText("Book Now");
     expect(headingElement).toBeInTheDocument();
+});
+
+test('Submit button should be disabled when form is invalid', () => {
+    render(<BookingForm state={initialState} dispatch={() => {}} onSubmit={() => {}} />);
+    const submitButton= screen.getByText("Make Your Reservation");
+    expect(submitButton).toBeDisabled();
 });
