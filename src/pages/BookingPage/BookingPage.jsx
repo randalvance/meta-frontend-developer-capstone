e13c0922f415b87afc15a012374216a726816648
Randal Vance Cunanan <randal@randalvance.net>
2024-08-02 10:34:37 +0800

    Use useReducer

--- src/pages/BookingPage/BookingPage.jsx
+++ src/pages/BookingPage/BookingPage.jsx
@@ -1,11 +1,45 @@
+import { useReducer } from 'react';
 import { BookingForm } from './components/BookingForm';
 
 
+const availableTimes = [
+    '17:00',
+    '18:00',
+    '19:00',
+    '20:00',
+    '21:00',
+    '22:00'
+];
+
+const initialState = {
+    date: new Date(),
+    time: '17:00',
+    guests: 1,
+    occasion: 'Birthday',
+    availableTimes,
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'SET_DATE':
+            return { ...state, date: new Date(action.payload) };
+        case 'SET_TIME':
+            return { ...state, time: action.payload };
+        case 'SET_GUESTS':
+            return { ...state, guests: action.payload };
+        case 'SET_OCCASION':
+            return { ...state, occasion: action.payload };
+        default:
+            return state;
+    }
+}
+
 export default function BookingPage() {
+    const [state, dispatch] = useReducer(reducer, initialState);
     return (
         <div>
             <h1>Booking Page</h1>
-            <BookingForm />
+            <BookingForm state={state} dispatch={dispatch} />
         </div>
     );
 }
--- src/pages/BookingPage/components/BookingForm.jsx
+++ src/pages/BookingPage/components/BookingForm.jsx
@@ -1,13 +1,4 @@
-import { useMemo, useState } from 'react';
-
-const availableTimes = [
-    '17:00',
-    '18:00',
-    '19:00',
-    '20:00',
-    '21:00',
-    '22:00'
-];
+import PropTypes from 'prop-types';
 
 function formatDate(date) {
     const year = date.getFullYear();
@@ -16,41 +7,31 @@ function formatDate(date) {
     return `${year}-${month}-${day}`;
 }
 
-export const BookingForm = () => {
-    const [date, setDate] = useState(new Date());
-    const [time, setTime] = useState('17:00');
-    const [guests, setGuests] = useState(1);
-    const [occasion, setOccasion] = useState('Birthday');
-
-    const formattedDate = useMemo(() => formatDate(date), [date]);
-
+export const BookingForm = ({ state, dispatch }) => {
     return (
         <form style={{ display: 'grid', maxWidth: "200px", gap: "20px" }}>
             <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date" value={formattedDate} onChange={(e) => {
-                console.log(e.target.value);
-                setDate(new Date(e.target.value));
-            }} />
+            <input type="date" id="res-date" value={formatDate(state.date)} onChange={(e) => dispatch({ type: 'SET_DATE', payload: e.target.value })} />
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time" value={time} onChange={(e) => {
-                setTime(e.target.value);
-            }}>
+            <select id="res-time" value={state.time} onChange={(e) => dispatch({ type: 'SET_TIME', payload: e.target.value })}>
                 {
-                    availableTimes.map(timeOption => <option key={timeOption}>{timeOption}</option>)
+                    state.availableTimes.map(timeOption => <option key={timeOption}>{timeOption}</option>)
                 }
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" value={guests} onChange={(e) => {
-                setGuests(e.target.value);
-            }} />
+            <input type="number" placeholder="1" min="1" max="10" id="guests" value={state.guests} onChange={(e) => dispatch({ type: 'SET_GUESTS', payload: e.target.value })} />
             <label htmlFor="occasion">Occasion</label>
-            <select id="occasion" value={occasion} onChange={(e) => {
-                setOccasion(e.target.value);
-            }}>
+            <select id="occasion" value={state.occasion} onChange={(e) => dispatch({ type: 'SET_OCCASION', payload: e.target.value })}>
                 <option value="Birthday">Birthday</option>
                 <option value="Anniversary">Anniversary</option>
             </select>
             <input type="submit" value="Make Your reservation" />
         </form>
     )
+};
+
+
+BookingForm.propTypes = {
+    state: PropTypes.object.isRequired,
+    dispatch: PropTypes.func.isRequired,
 };
